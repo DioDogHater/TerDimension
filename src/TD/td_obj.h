4f8bd5cb619e9811c7742cac810e49e14e312939
DioDogHater <epiccoder2008@gmail.com>
2025-07-25 11:26:57 -0400

    Backup commit

--- src/TD/td_obj.h
+++ src/TD/td_obj.h
@@ -7,4 +7,14 @@
 #include "terdimension.h"
 #include "../fast_obj.h"
 
+// Function to parse and transform a .obj file into a mesh
+TD_FUNC TD_Mesh TD_load_obj(const char* filepath){
+	fastObjMesh* obj = fast_obj_read(filepath);
+
+}
+
+TD_FUNC void TD_destroy_obj(TD_Mesh* m){
+	
+}
+
 #endif
